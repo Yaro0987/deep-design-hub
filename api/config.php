11b4ac4601316d76c2ec6f8d4b75bd07f2c97d47
Yaro0987 <abubakarmusa0987@gmail.com>
2026-07-12 10:30:51 +0100

    Remove invalid Allow-Credentials with wildcard origin

--- api/config.php
+++ api/config.php
@@ -12,7 +12,6 @@ if (in_array($origin, $allowed)) {
 }
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, X-Requested-With');
-header('Access-Control-Allow-Credentials: true');
 
 if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
